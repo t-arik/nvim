b6a7ee1a14664f9070dc77a43cac5356fdbabdfb
Tarik <44213832+t-arik@users.noreply.github.com>
2024-03-27 21:11:23 +0100

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,7 @@
 return {
   {
     "morhetz/gruvbox",
-    config = function() vim.cmd.colorscheme("gruvbox") end,
+    config = function() vim.cmd.colorscheme("rose-pine") end,
   },
+  { "rose-pine/neovim", name = "rose-pine" }
 }
