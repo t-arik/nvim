bfc712dcaa0630c41bc77e2dda05e2577651c683
Tarik <tarik.badawy@studium.uni-hamburg.de>
2024-05-17 16:21:36 +0200

    trouble

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -6,14 +6,13 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     opts = {
-      on_attach = function(bufnr)
+      on_attach = function(_bufnr)
         local gs = package.loaded.gitsigns
         nmap('<leader>hs', gs.stage_hunk)
         nmap('<leader>hr', gs.reset_hunk)
         nmap('<leader>hu', gs.undo_stage_hunk)
         nmap('<leader>hp', gs.preview_hunk)
         nmap('<leader>hb', function() gs.blame_line{full=true} end)
-        nmap('<leader>tb', gs.toggle_current_line_blame)
       end
     },
   }
