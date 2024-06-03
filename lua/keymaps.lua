30a4fa7fe4b99d83402e3e6f1b4af12e15204f5d
Tarik <44213832+t-arik@users.noreply.github.com>
2024-06-03 23:50:25 +0200

    fix: C-c to ESC mapping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,7 @@
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.api.nvim_set_keymap('c', '<C-c>', '<ESC>', { noremap = true })
+vim.keymap.set({ "n", "i", "c", "v", "o", "t" }, '<C-c>', '<ESC>', { noremap = true })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank() end,
